2671a57ad36d55612961bea1ea9aaf3bba01c99d
Ethan Pippin <ethanpippin2343@gmail.com>
2024-11-24 21:19:55 -0500

    init

---
+++ WhopTakeHome/WhopTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhopTakeHomeApp.swift
+//  WhopTakeHome
+//
+//  Created by Ethan Pippin on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WhopTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhopTakeHomeTests/WhopTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhopTakeHomeTests.swift
+//  WhopTakeHomeTests
+//
+//  Created by Ethan Pippin on 11/24/24.
+//
+
+import Testing
+@testable import WhopTakeHome
+
+struct WhopTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
